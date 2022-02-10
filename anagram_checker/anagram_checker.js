6ae8bf4b1ff7932766ebadcd5cd9ebd8e633e00c
UllernJ <Ulrik.johannesen@gmail.com>
2022-02-10 15:24:37 +0100

    Lagt til i egne mapper.

---
+++ anagram_checker/anagram_checker.js
@@ -0,0 +1,30 @@
+//vi lager en funksjon som fjerner alle duplikat bokstaver.
+const unrepeated = (str) => [... new Set(str)].join("");
+
+//vi bruker funksjonen og fjerner unødvendige mellom rom og punktum.
+var string1 = unrepeated("darez").replace(" ", "").replace(".", "");
+//vi sorterer for å få en rekkefølge i arrayet.
+var array = Array.from(string1).sort()
+
+var string2 = unrepeated("readz").replace(" ", "").replace(".", "");
+var array2 = Array.from(string2).sort();
+
+//vi sender verdiene våre til en function som sjekker om stringene er anagram.
+
+if(array.length == array2.length) {
+    sjekkOmAnagram(array, array2);
+}
+else {
+    console.log("Dette er ikke et anagram.")
+}
+function sjekkOmAnagram(x, y) {
+    for(i = 0; i < x.length; i++) {
+        if(x[i] !== y[i]) {
+            return false;
+        }
+    }
+    return true;
+}
+console.log(sjekkOmAnagram(array, array2))
+
+
